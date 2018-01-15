e32cc93dc99b3b695b5bbed264d94b09a1c10c8e
Trystan Sykes <trystansykes@gmail.com>
2018-01-16 09:29:20 +1100

    added params for listStacks

--- vpc-lambda.js
+++ vpc-lambda.js
@@ -5,7 +5,9 @@ exports.handler = (event, context, callback) => {
     var bucket = event.Records[0].s3.bucket.name
     var key = event.Records[0].s3.object.key
     var cloudformation = new AWS.CloudFormation();
-    var params = {}
+    var params = {
+        StackStatusFilter: ["CREATE_COMPLETE", "ROLLBACK_COMPLETE", "UPDATE_COMPLETE"]
+    }
     cloudformation.listStacks(params, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
