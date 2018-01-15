a183bface55a06fc212c4641f0d2dd502220d2b0
Trystan Sykes <trystansykes@gmail.com>
2018-01-15 16:26:47 +1100

    variable scope was incorrect, fixed

--- vpc-lambda.js
+++ vpc-lambda.js
@@ -1,5 +1,5 @@
 var AWS = require('aws-sdk');
-const stackMessage = []
+const stackMessage = ''
 exports.handler = (event, context, callback) => {
     var region = event.Records[0].awsRegion
     var bucket = event.Records[0].s3.bucket.name
@@ -14,16 +14,16 @@ exports.handler = (event, context, callback) => {
             cloudformation.createStack(params, function (err, data) {
                 if (err) {
                     console.log(err, err.stack);  
-                    stackMessage.push('stack is up to date')
+                    stackMessage = 'stack is up to date'
                 } else {
                     console.log(data);
-                    stackMessage.push('creating stack')
+                    stackMessage = 'creating stack'
                 }     
             });
         } else {
             console.log(data);
-            stackMessage.push('updating stack')
+            stackMessage = 'updating stack'
         }
     });
-    callback(null, stackMessage[0]);
+    callback(null, stackMessage);
 };
