5381446369d53ca3c1c360872fab5796f4e7e31b
Trystan Sykes <trystansykes@gmail.com>
2018-01-15 12:03:17 +1100

    added cloudformation template and node lambda to repo

---
+++ vpc-lambda.js
@@ -0,0 +1,16 @@
+var AWS = require('aws-sdk');
+exports.handler = (event, context, callback) => {
+    var region = event.Records[0].awsRegion
+    var bucket = event.Records[0].s3.bucket.name
+    var key = event.Records[0].s3.object.key
+    var cloudformation = new AWS.CloudFormation();
+    var params = {
+        StackName: "ts-test-vpc-jenkins",
+        TemplateURL: `https://s3-${region}.amazonaws.com/${bucket}/${key}`
+    }
+    cloudformation.createStack(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
+    callback(null, 'attempting stack creation');
+};
