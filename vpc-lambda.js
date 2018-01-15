de787ac49a78d752981a2d7b0f4d00cc49e5554e
Trystan Sykes <trystansykes@gmail.com>
2018-01-15 15:27:42 +1100

    fixed formatting

--- vpc-lambda.js
+++ vpc-lambda.js
@@ -11,9 +11,9 @@ exports.handler = (event, context, callback) => {
     cloudformation.updateStack(params, function (err, data) {
         if (err) {
             cloudformation.createStack(params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-    });
+                if (err) console.log(err, err.stack); // an error occurred
+                else     console.log(data);           // successful response
+            });
         } // an error occurred
         else     console.log(data);           // successful response
     });
