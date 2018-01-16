4217bfe013d74c1915f9f60d09ce2097c5e2db34
Trystan Sykes <trystansykes@gmail.com>
2018-01-16 11:16:46 +1100

    commented out list stacks

--- vpc-lambda.js
+++ vpc-lambda.js
@@ -5,13 +5,13 @@ exports.handler = (event, context, callback) => {
     var bucket = event.Records[0].s3.bucket.name
     var key = event.Records[0].s3.object.key
     var cloudformation = new AWS.CloudFormation();
-    var params = {
-        StackStatusFilter: ["CREATE_COMPLETE", "ROLLBACK_COMPLETE", "UPDATE_COMPLETE"]
-    }
-    cloudformation.listStacks(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-    });
+    // var params = {
+    //     StackStatusFilter: ["CREATE_COMPLETE", "ROLLBACK_COMPLETE", "UPDATE_COMPLETE"]
+    // }
+    // cloudformation.listStacks(params, function(err, data) {
+    //     if (err) console.log(err, err.stack); // an error occurred
+    //     else     console.log(data);           // successful response
+    // });
     params = {
         StackName: "ts-test-vpc-jenkins",
         TemplateURL: `https://s3-${region}.amazonaws.com/${bucket}/${key}`
